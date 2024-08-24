a4cd00d481c31acf3afe864a35d5f2964fefb0b0
herecomesfed <federicograziani97@gmail.com>
2024-08-24 10:04:32 +0200

    Cursor pointer

--- src/components/OptionsContainer.tsx
+++ src/components/OptionsContainer.tsx
@@ -23,18 +23,16 @@ import { ReactNode } from "react";
 export default function OptionsContainer({
   children,
   onClick,
-  // data,
   selected = false,
 }: {
   children: ReactNode;
   onClick: () => void;
-  // data: plansData | addonsData;
   selected: boolean;
 }) {
   return (
     <div
       onClick={onClick}
-      className={`[&:not(last-child)]:mb-3 p-3 border rounded-lg flex-grow ${
+      className={`[&:not(last-child)]:mb-3 p-3 border rounded-lg flex-grow cursor-pointer ${
         selected ? "bg-magnolia border-purplish-blue" : "border-cool-gray"
       }`}
     >
