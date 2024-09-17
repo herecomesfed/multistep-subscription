461051472acd81cbc52843a9b8ffdb5453cb8b14
herecomesfed <federicograziani97@gmail.com>
2024-09-17 23:43:22 +0200

    Implement form validation for your-info page

--- src/components/Bottombar.tsx
+++ src/components/Bottombar.tsx
@@ -1,7 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import Button from "./Button";
-import { ReactElement, useContext } from "react";
+import { ReactElement, useCallback, useContext, useEffect } from "react";
 import { contextTypes, DataContext } from "../context/DataContext";
+import { FormContext, formContextType } from "../context/FormContext";
 
 interface stageTypes {
   name: string;
@@ -16,6 +17,13 @@ export default function Bottombar({ stages }: { stages: stageTypes[] }) {
     isConfirmed: contextTypes["isConfirmed"];
     setIsConfirmed: contextTypes["setIsConfirmed"];
   };
+  const { formElements, handleValidate, isSubmitted, setIsSubmitted } =
+    useContext(FormContext) as {
+      handleValidate: formContextType["handleValidate"];
+      formElements: formContextType["formElements"];
+      isSubmitted: formContextType["isSubmitted"];
+      setIsSubmitted: formContextType["setIsSubmitted"];
+    };
   // Check the actual url using useLocation
   const location = useLocation();
   const { pathname } = location;
@@ -29,17 +37,35 @@ export default function Bottombar({ stages }: { stages: stageTypes[] }) {
   const nextComponent = stages.find(
     (stage) => currentComponent && stage.id === currentComponent.id + 1
   );
+  const formLocation = pathname === "/your-info";
 
   function handlePreviousStep() {
     if (!previousComponent) return;
     navigate(previousComponent.path);
   }
 
-  function handleNextStep() {
+  // Using function expression for the useCallback hook
+  const handleNextStep = useCallback(() => {
     if (!nextComponent) return;
+    if (formElements.some((element) => element.error)) return;
     navigate(nextComponent.path);
+  }, [formElements, navigate, nextComponent]);
+
+  // Submit form function
+  function handleSubmit() {
+    setIsSubmitted(true);
+    handleValidate();
   }
 
+  // Check for submit event and return if there are errors
+  useEffect(() => {
+    if (!isSubmitted) return;
+    if (formElements.some((element) => element.error)) return;
+    handleNextStep();
+    setIsSubmitted(false);
+  }, [formElements, isSubmitted, setIsSubmitted, handleNextStep]);
+
+  // Confirm order if last page
   function handleConfirmOrder() {
     setIsConfirmed(true);
   }
@@ -54,7 +80,11 @@ export default function Bottombar({ stages }: { stages: stageTypes[] }) {
         ) : (
           <span className="invisible"></span>
         )}
-        {nextComponent && <Button onClick={handleNextStep}>Next Step</Button>}
+        {nextComponent && (
+          <Button onClick={formLocation ? handleSubmit : handleNextStep}>
+            Next Step
+          </Button>
+        )}
         {!nextComponent && (
           <Button onClick={handleConfirmOrder}>Confirm</Button>
         )}
