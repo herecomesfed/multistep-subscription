c3c5009d8cdba70bc6e109bf0cfab3f9ea7f6d9a
herecomesfed <federicograziani97@gmail.com>
2024-09-17 23:44:13 +0200

    Implement form validation

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -5,6 +5,9 @@ export interface formElementsType {
   placeholder: string;
   type: string;
   label: string;
+  path?: RegExp;
+  error: boolean;
+  errorMessage: string;
 }
 
 export interface formValuesTypes {
@@ -15,41 +18,86 @@ export interface formValuesTypes {
 
 export interface formContextType {
   formElements: formElementsType[];
+  setFormElements: React.Dispatch<React.SetStateAction<formElementsType[]>>;
   formValues: formValuesTypes;
   setFormValues: React.Dispatch<React.SetStateAction<formValuesTypes>>;
+  handleValidate: () => void;
+  isSubmitted: boolean;
+  setIsSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const FormContext = createContext<formContextType | null>(null);
 
-const formElements = [
-  {
-    name: "your-name",
-    placeholder: "e.g. Stephen King",
-    type: "text",
-    label: "Name",
-  },
-  {
-    name: "your-email",
-    placeholder: "stephenking@lorem.com",
-    type: "email",
-    label: "Email Address",
-  },
-  {
-    name: "your-phone",
-    placeholder: "+1 234 567 890",
-    type: "tel",
-    label: "Phone Number",
-  },
-];
-
 function FormProvider({ children }: { children: ReactNode }) {
+  const [formElements, setFormElements] = useState([
+    {
+      name: "your-name",
+      placeholder: "e.g. Stephen King",
+      type: "text",
+      label: "Name",
+      error: false,
+      errorMessage: "",
+    },
+    {
+      name: "your-email",
+      placeholder: "stephenking@lorem.com",
+      type: "email",
+      label: "Email Address",
+      path: /^[\w.-]+@[a-zA-Z\d-]+(\.[a-zA-Z]{2,})+$/,
+      error: false,
+      errorMessage: "",
+    },
+    {
+      name: "your-phone",
+      placeholder: "+1 234 567 890",
+      type: "tel",
+      label: "Phone Number",
+      path: /^\+?\d{1,3}?[-.\s]?\(?\d{1,4}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/,
+      error: false,
+      errorMessage: "",
+    },
+  ]);
+
   const [formValues, setFormValues] = useState({
     "your-name": "",
     "your-email": "",
     "your-phone": "",
   });
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function handleValidate() {
+    setFormElements((prevEls: formElementsType[]) => {
+      return prevEls.map((element) => {
+        const inputValue = formValues[element.name as keyof formValuesTypes];
+        if (
+          inputValue === "" ||
+          (element.path && !element.path.test(inputValue))
+        ) {
+          return {
+            ...element,
+            error: true,
+            errorMessage: `Please insert a valid ${element.label}`,
+          };
+        } else {
+          return { ...element, error: false, errorMessage: "" };
+        }
+      });
+    });
+  }
+
   return (
-    <FormContext.Provider value={{ formElements, formValues, setFormValues }}>
+    <FormContext.Provider
+      value={{
+        formElements,
+        setFormElements,
+        formValues,
+        setFormValues,
+        handleValidate,
+        isSubmitted,
+        setIsSubmitted,
+      }}
+    >
       {children}
     </FormContext.Provider>
   );
